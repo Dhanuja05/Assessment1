ec96924ac44808a394e5458511b3a490ab674e54
Dhanuja05 <dhanu.doodleblue@gmail.com>
2022-04-03 22:29:47 +0530

    'pro'

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Project1 from './Project1'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Project1/>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/Project1.js
@@ -0,0 +1,524 @@
+import React from "react";
+import { GoMail } from "react-icons/go";
+import { FiPhoneCall } from "react-icons/fi";
+import { TiSocialGooglePlusCircular } from "react-icons/ti";
+import { TiSocialPinterestCircular } from "react-icons/ti";
+import { TiSocialTwitterCircular } from "react-icons/ti";
+import { TiSocialDribbble } from "react-icons/ti";
+import wp from './assets/wp.png';
+import { AiOutlineLeft } from "react-icons/ai";
+import { AiOutlineRight } from "react-icons/ai";
+import third from './assets/third.png';
+import { MdOutlineSquareFoot } from "react-icons/md";
+import { FaBath } from "react-icons/fa";
+import { IoBedOutline } from "react-icons/io5";
+import { AiOutlineSwap } from "react-icons/ai";
+import { AiOutlineHeart } from "react-icons/ai";
+import building2 from './assets/building2.jpg';
+import Georgia from './assets/Georgia.jpg';
+import geor1 from './assets/geor1.jpg';
+import ent2 from './assets/ent2.webp';
+import sign from './assets/sign.png';
+import { ImQuotesLeft } from "react-icons/im";
+import line from './assets/line.png';
+import people1 from './assets/people1.jpg';
+import female1 from './assets/female1.jpg';
+import male2 from './assets/male2.jpg';
+import female2 from './assets/female2.jpg';
+import male1 from './assets/male1.jpg';
+import { BsDot } from "react-icons/bs";
+import house3 from './assets/house3.jpg';
+import house2 from './assets/house2.jpg';
+import house4 from './assets/house4.jpg';
+import house1 from './assets/house1.jpg';
+import house5 from './assets/house5.jpg';
+import pic1 from './assets/pic1.png';
+import pic2 from './assets/pic2.png';
+import { TiSocialDribbbleCircular } from "react-icons/ti";
+import { AiFillBehanceCircle } from "react-icons/ai";
+import { RiFacebookCircleLine } from "react-icons/ri";
+import './Project1.css';
+function Project1() {
+    return (
+        <div>
+            <div className="a">
+                <div className="b">
+                    <GoMail size='30' />
+                    <div className="c">
+                        sample@sample.com </div>
+                    <div className="d">
+                        <FiPhoneCall size='25' />
+                        <div className="e">00 1234 5678</div>
+                    </div>
+                    <div className="f">
+                        <TiSocialGooglePlusCircular size='40' />
+                    </div>
+                    <div className="g">
+                        <TiSocialPinterestCircular size='40' />
+                    </div>
+                    <div className="h">
+                        <TiSocialTwitterCircular size='40' />
+                    </div>
+                    <div className="i">
+                        <TiSocialDribbble size='40' />
+                    </div>
+                </div>
+            </div>
+            <div className="wrapper">
+                <div className="j">
+                    <img src={wp} />
+                </div>
+
+                <div className="k">
+                    HOME
+
+                </div>
+                <div className="l">
+                    ABOUT
+                </div>
+                <div className="m">
+                    AGENTS
+                </div>
+                <div className="n">
+                    PROPERTIES
+                </div>
+                <div className="o">
+                    BLOG
+                </div>
+                <div className="p">
+                    CONTACTS
+                </div>
+
+            </div>
+
+            <div className="q">
+                <div className="r">
+                    if u can dream u can , we can create it
+                </div>
+                <div className="s">
+                    Find your house without
+                </div>
+                <div className="t">
+                    any difficulties
+                </div>
+                <br />
+                <div className="u">
+
+                    <button className="btn btn-outline-secondary  text-primary mb-4">Know More</button>
+                </div>
+                <div className="u1">
+                    01
+                    <div className="u2">
+                        |  05
+
+
+                    </div>
+                    <div className="u3">
+                        . . . . .
+                    </div>
+                    <div className="u4">
+                        < AiOutlineLeft size='15' /></div>
+                    <div className="u5">
+                        <AiOutlineRight size='15' />
+                    </div>
+
+                </div>
+            </div>
+            <div className="v">
+                <img src={third} />
+            </div>
+            <div className="w">
+                <img src={wp} />
+            </div>
+            <div className="x">
+                Properties
+            </div>
+            <div className="y">
+                HOME
+                <div className="y1">
+                    FOR
+                </div>
+                <div className="y2">
+                    SALE
+                </div>
+
+            </div>
+            <div className="z">
+                <div className="z1">
+                    Scandinavian Style Apartment
+
+                    <div className="z2">
+                        94-98 Ingraham St,Brooklyn,NY 11237,USA</div>
+                    <div className="z3">
+                        <div className="z4">
+                            <IoBedOutline size='20' /></div>
+                        Beds:3
+                        <div className="z5">
+                            <div className="z6">
+                                <FaBath /></div>
+                            Baths:2
+                        </div>
+                        <div className="z7">
+                            <MdOutlineSquareFoot size='25' />
+                            Sq.Ft:900
+                        </div>
+
+                    </div>
+                    <hr></hr>
+
+                    <div className="z8">
+                        FOR RENT
+                        <br />
+
+                        $54.000
+
+                        <div className="z9">
+                            <AiOutlineSwap size='20' /></div>
+                        <div className="z10">
+                            <AiOutlineHeart size='20' /></div>
+                    </div>
+                </div>
+
+
+            </div>
+            <div className="A">
+                <img src={building2} width='400' />
+                <img src={Georgia} width='400' height='200' />
+                <img src={geor1} width='400' height='220' />
+            </div>
+            <div className="A7">
+                <div className="A1">
+                    Scandinavian Style Apartment</div>
+                <div className="A3">
+                    Lovelace Road Greenfield
+                </div>
+                <div className="A5">
+                    Building Complex Apartments
+                </div>
+            </div>
+            <div className="A8">
+                <div className="A2">
+                    94-98 Ingraham St,Brooklyn,NY 11237,USA</div>
+
+
+
+                <br />
+                <div className="A4">
+                    120 N St  SW Thomaston, Georgia(GA), 30286
+                </div>
+
+
+                <div className="A6">
+                    319 Brookmeade Way Lawrenceville, Georgia(GA),30043
+                </div>
+            </div>
+
+            <hr className="B"></hr>
+
+            <div>
+                <button className="C">+</button>
+            </div>
+            <div className="D">
+                VIEW MORE
+            </div>
+            <br />
+            <br />
+            <br />
+            <div className="E">
+                <div className="E1">
+                    <h1>Beautifully designed , inside<br />
+                        and out , Studio looks every bit <br />
+                        as powerful as it is.</h1>
+                </div>
+
+                <div className="E2">
+                    <img src={ent2} width='400' height='500' /></div>
+            </div>
+            <div className="E3">
+                This is photoshop's versio of lorem ipsum.proin gravida nibh vel velit auctor aliquest.<br />
+                Aenean sollicitidin,lorem quin bibendum auctor,nsi elit consequat ,nec segithis sem <br />
+                nibh id llit.Duis sed odio sit ament nibh vulputate curus a sit amet mauris.
+            </div>
+            <div className="E4">
+                <img src={sign} />
+            </div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <div className="F">
+                <div className="F1">
+                    Our </div>
+                <div className="F2">
+                    Clients Review
+                </div>
+            </div>
+            <br />
+            <br />
+            <div className="G">
+                Duane Neal  </div>
+            <div className="G1">
+                Product Designer,USA
+            </div>
+            <div className="H">
+                <div className="H1">
+                    <ImQuotesLeft size='25' />
+                </div>
+                <div className="H2">
+                    I'm a freelance designer who specialises in print designs and combining fabrics.My designs have been sold all over Europe and<br />
+                    the USA and I have worked with some of the biggest designers in the industry. I'm also currently working with abig supermarket to
+                </div>
+                <div className='H3'>
+                    develop a gorgeous, yet affordable
+                </div>
+            </div>
+            <br />
+            <br />
+            <br />
+            <div className="I">
+                <img src={line} />
+            </div>
+            <div className="J">
+                <div className="J1">
+                    <img src={people1} width='100' height='100' /></div>
+                <div className="J2">
+                    <img src={female1} width='100' height='100' />
+                </div>
+                <div className="J3">
+                    <img src={male2} width='100' height='100' />
+                </div>
+                <div className="J4">
+                    <img src={female2} width='100' height='100' />
+                </div>
+                <div className="J5">
+                    <img src={male1} width='100' height='100' />
+                </div>
+            </div>
+            <div className="K">
+                <div className="K1">
+                    <BsDot size='40' /> </div>
+                <div className="K2">
+                    <BsDot size='40' /> </div>
+                <div className="K3">
+                    <BsDot size='40' />
+                </div>
+                <div className="K4"> <BsDot size='40' /></div>
+                <div className="K5">
+                    <BsDot size='40' />
+                </div>
+            </div>
+            <div className="L">
+                <div className="L1">
+                    Our</div><div className="L2"> Blog</div>
+            </div>
+            <div className="M">
+                <div className="M1">
+                    <img src={house3} height='750' width='525' className="demo" />
+                    <div className="M1a">
+                        Lorem Ipsum is simple dummy text of <br />the printing and typesetting
+                        <hr className="M1b"></hr>
+                        <div className="M1c">
+                            13 March 2020
+                        </div>
+                        <div>
+                            Countrary to popular belief,Lorem ipsum is not simply random text it <br />has roots in a piece of classical Latin literature from
+                        </div>
+                    </div>
+
+                </div>
+                <div className="M2">
+
+                    <img src={house2} height='350' width='250' />
+                    <div className="M2a">
+                        There are many varients of<br /> passangers of lorem ipsum
+                        <hr className="M2b"></hr>
+                        <div className="M2c">
+                            13 March 2020
+                        </div>
+                    </div>
+
+                </div>
+                <div className="M3">
+                    <img src={house4} height='340' width='250' />
+                    <div className="M3a">
+                        There are many varients of<br /> passangers of lorem ipsum
+                        <hr className="M3b"></hr>
+                        <div className="M3c">
+                            13 March 2020
+                        </div>
+                    </div>
+                </div>
+                <div className="M4">
+                    <img src={house1} height='350' width='250' />
+                    <div className="M4a">
+                        There are many varients of<br /> passangers of lorem ipsum
+                        <hr className="M4b"></hr>
+                        <div className="M4c">
+                            13 March 2020
+                        </div>
+                    </div>
+                </div>
+                <div className="M5">
+                    <img src={house5} height='350' width='250' />
+                    <div className="M5a">
+                        There are many varients of<br /> passangers of lorem ipsum
+                        <hr className="M5b"></hr>
+                        <div className="M5c">
+                            13 March 2020
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br />
+            <br />
+            <br />
+            <div className="N">
+                <div className="N1">
+                    Everything is about to change.</div>
+
+                <div className="N2">
+                    Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    <div className="N3">
+                        minim veniam, quis nostrud exercitation ullamco laboris.
+                    </div>
+
+                </div>
+                <div className="N4">
+                    <input type='text' className="form-control w-50 p-2" placeholder="Email Address"></input>
+                    <div className="N5">
+                        <button className="N6"> SUBSCRIBE</button></div>
+                </div>
+            </div>
+
+            <div className="O">
+                <div className="O1">
+                    <div className="O2">
+                        wp
+                    </div>
+                    <div className="O3">
+                   /
+                    </div>
+                    <div className="O4">
+                        Real Estate
+                    </div>
+                </div>
+                <div className="O5">
+                   <div className="O5a">
+                       HOME
+                   </div>
+                   <div className="O5b">
+                       ABOUT
+                   </div>
+                   <div className="O5c">
+                       AGENT
+                   </div>
+                 <div className="O5d">
+                     PROPERTIES
+                 </div>
+                 <div className="O5e">
+                    BLOG
+                 </div>
+                 <div className="O5f">
+                     CONTACT
+                 </div>
+                </div>
+            </div>
+
+            <div className="P">
+                <div className="P1">
+                ABOUT
+                 <div className="P1a">
+                    <div className="P1b">
+                  ABOUT STUDIO</div>
+                 <div className="P1c">
+                 JOBS</div>
+                 <div className="P1d">
+                 TEAM</div>
+                 <div className="P1e">
+                 SECURITY</div>
+                 </div>
+                </div>
+                <div className="P2">
+                INFORMATION
+                <div className="P2a">
+                    <div className="P2b">
+                  CUSTOMER SERVICE</div>
+                 <div className="P2c">
+                 LEGAL INFORMATION</div>
+                 <div className="P2d">
+                 SUPPORT </div>
+                 <div className="P2e">
+                 FAQ</div>
+                 </div>
+
+                </div>
+                <div className="P3">
+                
+                RECENT NEWS</div>
+                <div className="P3a">
+                    <img  src={pic1}/>  
+                    <div className="P3b">Pron eros nisl,omare sed
+                    <br/>vehicula sit amet feugiat</div>
+                    <div className="P3c">
+                        Aug 12 | 2017
+                    </div >
+                    <div className="P3d">
+                    <img src={pic2}/></div>
+                    <div className="P3e">
+                          Morbi placerat accumsan <br/>nunc,luctus lacinia nisi
+                    </div>
+                    <div className="P3f">
+                       Aug 12 | 2017
+                    </div>
+                </div>
+                <div className="P4">
+                GET IN TOUCH</div>
+                    <div className="P4a">
+                <BsDot  size='30'/>
+                <div className="P4b">
+                San Fransisco,CA<br/>
+                125,california,STP 2 Building</div>
+                <div className="P4c">
+                  <BsDot size='30'/>
+                  <div className="P4d">
+                  www.studiotheme.com
+                  </div>
+                </div>
+                <div className="P4e">
+                    <BsDot size='30'/>
+                    <div className="P4f">
+                        +123456789
+                    </div>
+                </div>
+                
+                </div>
+               
+            </div>
+            <div className="Q">
+                <div  className="Q1">
+                <TiSocialDribbbleCircular size='40'/>
+                <div className="Q2">
+                <RiFacebookCircleLine  size='35'/></div>
+                <div className="Q3">
+                <TiSocialTwitterCircular  size='40'/>
+                </div>
+                <div className="Q4">
+                <AiFillBehanceCircle  size='30'/>
+                </div>
+                </div>
+                <div className="Q5">
+                    PRIVACY POLICY 
+                    <div className="Q5a">
+                        |
+                    </div>
+                    <div className="Q5b">
+                        TERMS & CONDITIONS
+                    </div>
+                </div>
+                
+            </div>
+           
+        </div>
+    )
+}
+export default Project1
